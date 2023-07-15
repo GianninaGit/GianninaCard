f942b66a4d1686042cadb5d9704b886884711409
Giannina <giannisarappa@gmail.com>
2023-07-15 22:36:49 +0200

    Initial Commit

---
+++ GianninaCard/GianninaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GianninaCardApp.swift
+//  GianninaCard
+//
+//  Created by Giannina on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GianninaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
